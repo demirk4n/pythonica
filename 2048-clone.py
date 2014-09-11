8789093f22c0b4085eb8f13c20b73489f80a9f4a
Evren Demirkan <evren.demirk4n@gmail.com>
2014-09-11 07:16:26 +0300

    Game Class improved, some methods move out of Board Class

--- 2048-clone.py
+++ 2048-clone.py
@@ -1,14 +1,20 @@
-import pygame
-import random
+import pygame, random, time, sys
 from pygame.locals import *
 
 NUM_ROWS = 4  # y
 NUM_COLS = 4  # x
-
-CELL_SIZE = 60 # Cell size in pixels
+CELL_SIZE = 60  # Cell size in pixels
 TOP_SCREEN_MARGIN = 50
-# set up the colors R G B
+WINDOW_WIDTH = NUM_COLS * CELL_SIZE
+WINDOW_HEIGHT = NUM_ROWS * CELL_SIZE + TOP_SCREEN_MARGIN
 
+SCORE_FONT_SIZE = 30
+CELL_FONT_SIZE = 20
+
+FPS = 30  # Game FPS
+CELL_MOVE_SPEED = 10
+
+# set up the colors R G B
 BLACK = (0, 0, 0)
 GRAY = (100, 100, 100)
 NAVY_BLUE = ( 60,  60, 100)
@@ -28,7 +34,7 @@ UP = 'up'
 DOWN = 'down'
 
 
-class Board():
+class Board(object):
     def __init__(self, screen):
         self.board = {}
         self.create_board()
@@ -38,6 +44,7 @@ class Board():
         self.move_count = 0
         self.score = 0
         self.game_over = False
+        self.game_win = False
 
     def create_board(self):
         for x in range(NUM_ROWS):
@@ -58,25 +65,11 @@ class Board():
             rand_xy = random.choice(available_cell_list)
             self.board[rand_xy] = Cell(rand_xy, 2, screen, False)
 
-    def update(self, event, dt):
-        if event.type == pygame.KEYDOWN and event.key == pygame.K_LEFT:
-            self.shift_direction = LEFT
-            print("left pressed")
-            self.shift_board()
-        if event.type == pygame.KEYDOWN and event.key == pygame.K_RIGHT:
-            self.shift_direction = RIGHT
-            print("right pressed")
-            self.shift_board()
-        if event.type == pygame.KEYDOWN and event.key == pygame.K_UP:
-            self.shift_direction = UP
-            print("up pressed")
-            self.shift_board()
-        if event.type == pygame.KEYDOWN and event.key == pygame.K_DOWN:
-            self.shift_direction = DOWN
-            print("down pressed")
-            self.shift_board()
+    def update(self, shift_direction, dt):
+        self.shift_direction = shift_direction
+        self.shift_board()
 
-    def draw_board(self, dt):
+    def draw(self, display):
         screen.fill(BLACK)
         for xy, cell in self.board.items():
             cell.draw_cell(xy, cell.value)
@@ -88,14 +81,6 @@ class Board():
         self.move_count = 0
         #print(self.score)
 
-        # draw score
-        score_rect = pygame.rect.Rect((0, 0), (NUM_COLS * CELL_SIZE, TOP_SCREEN_MARGIN))
-        font = pygame.font.Font('freesansbold.ttf', 30)
-        text = font.render(str(self.score), True, WHITE)
-        text_pos = text.get_rect(center=(score_rect.centerx,score_rect.centery))
-        screen.blit(text, text_pos)
-
-
     def shift_order(self):
             order = []
             if self.shift_direction == LEFT:
@@ -106,7 +91,7 @@ class Board():
                 #return [(0,0),(1,0),(2,0),(3,0),(0,1),(1,1),(2,1),(3,1),(0,2),(1,2),(2,2),(3,2),(0,3),(1,3),(2,3),(3,3)]
             if self.shift_direction == RIGHT:
                 for y in range(NUM_ROWS):
-                    for x in range(NUM_COLS-1, -1 , -1):
+                    for x in range(NUM_COLS-1, -1, -1):
                         order.append((x,y))
                 return order
                 #return [(3,0),(2,0),(1,0),(0,0),(3,1),(2,1),(1,1),(0,1),(3,2),(2,2),(1,2),(0,2),(3,3),(2,3),(1,3),(0,3)]
@@ -167,14 +152,17 @@ class Board():
             else:
                 pass
 
+    def tick(self, dt):
+        pass
 
-class Cell():
+class Cell(object):
     def __init__(self, cell_loc, value, screen, lock):
         self.cell_loc = cell_loc
         self.value = value
         self.lock = lock
         self.x, self.y = cell_loc
         self.draw_cell(cell_loc, value)
+        self.offset = 0
 
     def draw_cell(self, cell_loc, value):
         cell_colors = {2: RED, 4: GREEN, 8: BLUE, 16: YELLOW, 32: ORANGE, 64: PURPLE, 128: CYAN, 256: NAVY_BLUE,
@@ -184,38 +172,76 @@ class Cell():
         if value >= 2:
             cell_rect = pygame.rect.Rect((loc_x, loc_y), (CELL_SIZE - 2, CELL_SIZE - 2))
             pygame.draw.rect(screen, cell_colors[value], cell_rect)
-            font = pygame.font.Font('freesansbold.ttf', 20)
+            font = pygame.font.Font('freesansbold.ttf', CELL_FONT_SIZE)
             text = font.render(str(value), True, WHITE)
             text_pos = text.get_rect(center=(cell_rect.centerx,cell_rect.centery))
             screen.blit(text, text_pos)
 
-class Game(object):
-    def main(self, screen):
-        clock = pygame.time.Clock()
-        board = Board(screen)
+    def tick(self, dt):
+            self.offset = max(0.0, self.offset - dt * CELL_MOVE_SPEED)
 
-        while 1:
-            dt = clock.tick(30)
+class Game(object):
+
+    def __init__(self):
+        self.board = None
+        pygame.init()
+        pygame.display.set_caption("Py2048 - by Evren")
+        self.clock = pygame.time.Clock()
+        self.display = pygame.display.set_mode((WINDOW_WIDTH, WINDOW_HEIGHT), DOUBLEBUF)
+        self.board = Board(screen)
+
+        #self.font = pygame.font.Font(None, FONT_SIZE)
+
+    def start(self):
+        pass
+
+    def quit(self):
+        pygame.quit()
+        sys.exit()
+
+    def play(self):
+        while True:
+            self.draw()
+            dt = min(self.clock.tick(FPS) / 1000.0, 1.0 / FPS)
 
             for event in pygame.event.get():
-                if event.type == pygame.QUIT:
-                    return
-                if event.type == pygame.KEYDOWN and event.key == pygame.K_ESCAPE:
-                    return
-                if event.type == pygame.KEYDOWN and event.key == pygame.K_LEFT:
-                    board.update(event, dt)
-                if event.type == pygame.KEYDOWN and event.key == pygame.K_RIGHT:
-                    board.update(event, dt)
-                if event.type == pygame.KEYDOWN and event.key == pygame.K_UP:
-                    board.update(event, dt)
-                if event.type == pygame.KEYDOWN and event.key == pygame.K_DOWN:
-                    board.update(event, dt)
+                if event.type == KEYDOWN:
+                    self.input(event.key)
+                elif event.type == QUIT:
+                    self.quit()
 
-            board.draw_board(dt / 1000.)
-            pygame.display.flip()
+            self.board.tick(dt)
+
+
+    def draw(self):
+        self.board.draw(self.display)
+        self.draw_score()
+        #pygame.display.flip()
+        pygame.display.update()
+
+
+    def draw_score(self):
+        score_rect = pygame.rect.Rect((0, 0), (NUM_COLS * CELL_SIZE, TOP_SCREEN_MARGIN))
+        font = pygame.font.Font('freesansbold.ttf', SCORE_FONT_SIZE)
+        text = font.render(str(self.board.score), True, WHITE)
+        text_pos = text.get_rect(center=(score_rect.centerx,score_rect.centery))
+        screen.blit(text, text_pos)
+
+    def input(self, key):
+        dt = 0.0
+        if key == K_ESCAPE:
+            self.quit()
+        if key == pygame.K_LEFT:
+            self.board.update(LEFT, dt)
+        if key == pygame.K_RIGHT:
+            self.board.update(RIGHT, dt)
+        if key == pygame.K_UP:
+            self.board.update(UP, dt)
+        if key == pygame.K_DOWN:
+            self.board.update(DOWN, dt)
+
 
 if __name__ == '__main__':
     pygame.init()
-    screen = pygame.display.set_mode((NUM_COLS * CELL_SIZE, (NUM_ROWS * CELL_SIZE) + TOP_SCREEN_MARGIN))
-    Game().main(screen)
-
+    screen = pygame.display.set_mode((WINDOW_WIDTH, WINDOW_HEIGHT))
+    Game().play()
