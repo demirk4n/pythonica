ea74b946dafcb2fb31e2a9a756a96c5064c6442c
Evren Demirkan <evren.demirk4n@gmail.com>
2014-09-14 23:11:04 +0300

    impoved Board update and cell move animation

--- 2048-clone.py
+++ 2048-clone.py
@@ -11,8 +11,8 @@ WINDOW_HEIGHT = NUM_ROWS * CELL_SIZE + TOP_SCREEN_MARGIN
 SCORE_FONT_SIZE = 30
 CELL_FONT_SIZE = 20
 
-FPS = 30  # Game FPS
-CELL_MOVE_SPEED = 10
+FPS = 10  # Game FPS
+CELL_MOVE_SPEED = 10  # in frames per second
 
 # set up the colors R G B
 BLACK = (0, 0, 0)
@@ -35,88 +35,100 @@ DOWN = 'down'
 
 
 class Board(object):
-    def __init__(self, screen):
+    def __init__(self):
         self.board = {}
-        self.create_board()
-        self.pop_random()
-        self.pop_random()
         self.shift_direction = None
         self.move_count = 0
         self.score = 0
         self.game_over = False
         self.game_win = False
+        self.dt = 0.0
+        self.busy = False
 
-    def create_board(self):
+    def create(self):
         for x in range(NUM_ROWS):
             for y in range(NUM_COLS):
                 cell_loc = (x, y)
                 # print(cell_loc)
-                self.board[cell_loc] = Cell(cell_loc, 0, screen, False)
+                self.board[cell_loc] = Cell(cell_loc, 0, False)
 
     def pop_random(self):
         available_cell_list = []
         for (x, y), cell in self.board.items():
             if cell.value == 0: # If value < 2 then this is an empty cell
                 available_cell_list.append((x, y))
-
         if len(available_cell_list) == 0:
             return False
         else:
             rand_xy = random.choice(available_cell_list)
-            self.board[rand_xy] = Cell(rand_xy, 2, screen, False)
+            self.board[rand_xy] = Cell(rand_xy, 2, False)
 
-    def update(self, shift_direction, dt):
-        self.shift_direction = shift_direction
-        self.shift_board()
+    def update(self, dt):
+
+        if self.busy:
+            self.busy = False
+            self.shift_board()
+        elif not self.busy and self.move_count > 0:
+            self.pop_random()
+            self.move_count = 0
+
+        #print(self.busy)
 
     def draw(self, display):
-        screen.fill(BLACK)
-        for xy, cell in self.board.items():
-            cell.draw_cell(xy, cell.value)
-            cell.lock = False
-
-        if self.move_count > 0:
-            self.pop_random()
-
-        self.move_count = 0
-        #print(self.score)
+        display.fill(BLACK)
+        for i, c in self.board.items():
+            c.draw()
 
     def shift_order(self):
-            order = []
-            if self.shift_direction == LEFT:
-                for y in range(NUM_ROWS):
-                    for x in range(NUM_COLS):
-                        order.append((x,y))
-                return order
-                #return [(0,0),(1,0),(2,0),(3,0),(0,1),(1,1),(2,1),(3,1),(0,2),(1,2),(2,2),(3,2),(0,3),(1,3),(2,3),(3,3)]
-            if self.shift_direction == RIGHT:
-                for y in range(NUM_ROWS):
-                    for x in range(NUM_COLS-1, -1, -1):
-                        order.append((x,y))
-                return order
-                #return [(3,0),(2,0),(1,0),(0,0),(3,1),(2,1),(1,1),(0,1),(3,2),(2,2),(1,2),(0,2),(3,3),(2,3),(1,3),(0,3)]
-            if self.shift_direction == UP:
+        order = []
+        if self.shift_direction == LEFT:
+            for y in range(NUM_ROWS):
                 for x in range(NUM_COLS):
-                    for y in range(NUM_ROWS):
-                            order.append((x, y))
-                return order
-                #return [(0,0),(0,1),(0,2),(0,3),(1,0),(1,1),(1,2),(1,3),(2,0),(2,1),(2,2),(2,3),(3,0),(3,1),(3,2),(3,3)]
-            if self.shift_direction == DOWN:
-                for x in range(NUM_COLS):
-                    for y in range(NUM_ROWS-1, -1, -1):
-                        order.append((x,y))
-                return order
-                #return [(0,3),(0,2),(0,1),(0,0),(1,3),(1,2),(1,1),(1,0),(2,3),(2,2),(2,1),(2,0),(3,3),(3,2),(3,1),(3,0)]
+                    order.append((x,y))
+            #return [(0,0),(1,0),(2,0),(3,0),(0,1),(1,1),(2,1),(3,1),(0,2),(1,2),(2,2),(3,2),(0,3),(1,3),(2,3),(3,3)]
+        if self.shift_direction == RIGHT:
+            for y in range(NUM_ROWS):
+                for x in range(NUM_COLS-1, -1, -1):
+                    order.append((x,y))
+            #return [(3,0),(2,0),(1,0),(0,0),(3,1),(2,1),(1,1),(0,1),(3,2),(2,2),(1,2),(0,2),(3,3),(2,3),(1,3),(0,3)]
+        if self.shift_direction == UP:
+            for x in range(NUM_COLS):
+                for y in range(NUM_ROWS):
+                        order.append((x, y))
+            #return [(0,0),(0,1),(0,2),(0,3),(1,0),(1,1),(1,2),(1,3),(2,0),(2,1),(2,2),(2,3),(3,0),(3,1),(3,2),(3,3)]
+        if self.shift_direction == DOWN:
+            for x in range(NUM_COLS):
+                for y in range(NUM_ROWS-1, -1, -1):
+                    order.append((x,y))
+            #return [(0,3),(0,2),(0,1),(0,0),(1,3),(1,2),(1,1),(1,0),(2,3),(2,2),(2,1),(2,0),(3,3),(3,2),(3,1),(3,0)]
+        return order
 
     def shift_board(self):
-        # print(self.shift_order())
-        for (x, y) in self.shift_order():
-                cell = self.board[(x, y)]
-                if cell.value >= 2:# action only on nonempty cells
-                    self.cell_action((x, y))
+        #print(self.shift_order())
 
-    def cell_action(self, xy):
-        x, y = xy
+        for (x, y) in self.shift_order():
+            current_cell = self.board[(x, y)]
+            current_cell.lock = False
+            next_cell = self.get_next_cell((x, y))
+
+            if current_cell.value >= 2 and next_cell:  # action only on nonempty cells
+                if next_cell.value == 0:
+                    next_cell.value, next_cell.lock = current_cell.value, False
+                    current_cell.value, current_cell.lock = 0, False
+                    if self.get_next_cell(next_cell.location):
+                        self.busy = True
+
+                elif next_cell.value == current_cell.value and next_cell.lock is False:
+                    next_cell.value += current_cell.value
+                    next_cell.lock = True
+                    current_cell.value, current_cell.lock = 0, False
+                    self.score += next_cell.value + current_cell.value
+
+                self.move_count += 1
+
+    def get_next_cell(self, location):
+
+        x, y = location
         if self.shift_direction == LEFT:
             x -= 1
             if x < 0:
@@ -134,65 +146,56 @@ class Board(object):
             if y == NUM_ROWS:
                 y = NUM_ROWS - 1
 
-        current_cell = self.board[xy]
-        target_cell = self.board[(x, y)]
+        if (x, y) == location: # no move
+            return False
+        else:
+            return self.board[(x, y)]
 
-        if not (x, y) == xy:
-            if target_cell.value == 0:
-                self.board[(x, y)] = Cell((x, y), current_cell.value, screen, False)
-                self.board[xy] = Cell(xy, 0, screen, False)
-                self.cell_action((x, y))
-                self.move_count += 1
-            if target_cell.value == current_cell.value and target_cell.lock is False:
-                new_cell_value = target_cell.value + current_cell.value
-                self.board[(x, y)] = Cell((x, y), new_cell_value, screen, True)
-                self.board[xy] = Cell(xy, 0, screen, False)
-                self.score += new_cell_value
-                self.move_count += 1
-            else:
-                pass
 
     def tick(self, dt):
-        pass
+        self.dt = dt
+        #print("dt " + str(dt))
+
 
 class Cell(object):
-    def __init__(self, cell_loc, value, screen, lock):
-        self.cell_loc = cell_loc
+    def __init__(self, location, value, lock):
+        self.location = location
         self.value = value
         self.lock = lock
-        self.x, self.y = cell_loc
-        self.draw_cell(cell_loc, value)
+        self.x, self.y = location
         self.offset = 0
 
-    def draw_cell(self, cell_loc, value):
+    def draw(self):
+
         cell_colors = {2: RED, 4: GREEN, 8: BLUE, 16: YELLOW, 32: ORANGE, 64: PURPLE, 128: CYAN, 256: NAVY_BLUE,
                        512: GRAY}
-        x, y = cell_loc
+        x, y = self.location
         (loc_x, loc_y) = x * CELL_SIZE, (y * CELL_SIZE) + TOP_SCREEN_MARGIN
-        if value >= 2:
+        if self.value >= 2:
             cell_rect = pygame.rect.Rect((loc_x, loc_y), (CELL_SIZE - 2, CELL_SIZE - 2))
-            pygame.draw.rect(screen, cell_colors[value], cell_rect)
+            pygame.draw.rect(screen, cell_colors[self.value], cell_rect)
             font = pygame.font.Font('freesansbold.ttf', CELL_FONT_SIZE)
-            text = font.render(str(value), True, WHITE)
+            text = font.render(str(self.value), True, WHITE)
             text_pos = text.get_rect(center=(cell_rect.centerx,cell_rect.centery))
             screen.blit(text, text_pos)
 
-    def tick(self, dt):
-            self.offset = max(0.0, self.offset - dt * CELL_MOVE_SPEED)
 
 class Game(object):
 
     def __init__(self):
         self.board = None
         pygame.init()
-        pygame.display.set_caption("Py2048 - by Evren")
+        pygame.display.set_caption("Py2048 - by ED")
         self.clock = pygame.time.Clock()
         self.display = pygame.display.set_mode((WINDOW_WIDTH, WINDOW_HEIGHT), DOUBLEBUF)
-        self.board = Board(screen)
+        self.board = Board()
 
         #self.font = pygame.font.Font(None, FONT_SIZE)
 
     def start(self):
+        self.board.create()
+        self.board.pop_random()
+        self.board.pop_random()
         pass
 
     def quit(self):
@@ -200,6 +203,8 @@ class Game(object):
         sys.exit()
 
     def play(self):
+        self.start()
+
         while True:
             self.draw()
             dt = min(self.clock.tick(FPS) / 1000.0, 1.0 / FPS)
@@ -209,9 +214,8 @@ class Game(object):
                     self.input(event.key)
                 elif event.type == QUIT:
                     self.quit()
-
-            self.board.tick(dt)
-
+            self.board.update(dt)
+            self.clock.tick(FPS)
 
     def draw(self):
         self.board.draw(self.display)
@@ -228,18 +232,20 @@ class Game(object):
         screen.blit(text, text_pos)
 
     def input(self, key):
-        dt = 0.0
         if key == K_ESCAPE:
             self.quit()
-        if key == pygame.K_LEFT:
-            self.board.update(LEFT, dt)
-        if key == pygame.K_RIGHT:
-            self.board.update(RIGHT, dt)
-        if key == pygame.K_UP:
-            self.board.update(UP, dt)
-        if key == pygame.K_DOWN:
-            self.board.update(DOWN, dt)
-
+        if key == pygame.K_LEFT and not self.board.busy:
+            self.board.shift_direction = LEFT
+            self.board.busy = True
+        if key == pygame.K_RIGHT and not self.board.busy:
+            self.board.shift_direction = RIGHT
+            self.board.busy = True
+        if key == pygame.K_UP and not self.board.busy:
+            self.board.shift_direction = UP
+            self.board.busy = True
+        if key == pygame.K_DOWN and not self.board.busy:
+            self.board.shift_direction = DOWN
+            self.board.busy = True
 
 if __name__ == '__main__':
     pygame.init()
